1a36ab0d6db9722d407d8ee67fa8d71c0e3bc74a
piyush7987 <144139132+piyush7987@users.noreply.github.com>
2025-06-03 03:09:33 +0530

    Add files via upload

---
+++ InstaRideApplication.java
@@ -0,0 +1,13 @@
+package com.example.InstaRide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaRideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaRideApplication.class, args);
+	}
+
+}
